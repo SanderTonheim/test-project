d37557baa60d515a9d37f923e57facfe3862262c
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-19 16:00:14 +0200

    styles imported

--- pages/prosjektet.js
+++ pages/prosjektet.js
@@ -1,9 +1,4 @@
+import styles from '../styles/prosjektet.module.css'
 export default function Prosjektet() {
-	return (
-		<div className={styles.container}>
-			
-
-    </div>
-		
-	)
+	return <div className={styles.container}><h1>test</h1></div>
 }
