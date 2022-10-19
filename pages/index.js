b1d4571fb0c21decba642a58a1dc2669ee7f6dec
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-19 11:55:58 +0200

    ""

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<h1>Velkommen Velkommen</h1>
+			<h1>Velkommen </h1>
 		</div>
 	)
 }
