1daca541b2d31fd7a572b407a042476a1ad63b9f
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-19 09:45:54 +0200

    Default landingpage for profile page when no query is added in url

---
+++ pages/profile/index.js
@@ -0,0 +1,12 @@
+// Dette vises når en bruker ikke har noen ekstra parametere. Feks "side.no/brukere " ingen bruker parameter er satt inn.
+
+export default function ProfileDefault() {
+	return (
+		<>
+			<h1>
+				Ai! Her var det ingen profiler! <br />
+				Gå tilbake og prøv på nytt
+			</h1>
+		</>
+	)
+}
