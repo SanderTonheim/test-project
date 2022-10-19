bff3d50dea116fb7e80e5708e76f80e725fa375d
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-19 14:00:34 +0200

    Navar layout component added

---
+++ components/Layout.js
@@ -0,0 +1,10 @@
+import Navbar from './navbar'
+// templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
+export default function Layout({ children }) {
+	return (
+		<>
+			<Navbar />
+			<div>{children}</div>
+		</>
+	)
+}
