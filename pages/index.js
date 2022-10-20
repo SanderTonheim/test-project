27767d028003ea6993efe45b1f114b14371f619d
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-20 13:44:21 +0200

    Added styling for Footer.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
 		<>
+			<Head></Head>
 			<div className={styles.container}>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
+				<h1>Velkommen </h1>
 				<h1>Velkommen </h1>
 			</div>
 		</>
