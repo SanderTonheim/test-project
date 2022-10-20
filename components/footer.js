742a1a6e7ce3a050983348820824b1109993cc25
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-20 13:43:41 +0200

    Created footer component.

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,18 @@
+import Link from 'next/link'
 import styles from '../styles/Footer.module.css'
 export default function Footer() {
 	return (
-		<div className={styles.footer}>
-			<p>Test prosjekt </p>
-			<p>2022</p>
-			<p>Sander Tonheim</p>
+		<div className={styles.container}>
+			<div className={styles.footer}>
+				<p>Test prosjekt </p>
+				<p>2022</p>
+				<p>Sander Tonheim</p>
+			</div>
+			<div className={styles.links}>
+				<Link href={'/'}>Hjem</Link>
+				<Link href={'/about'}>about</Link>
+				<Link href={'/prosjektet'}>prosjektet</Link>
+			</div>
 		</div>
 	)
 }
