b5faa7b45f7cd79a30f82d591318405e34b2589d
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-20 13:43:03 +0200

    removed the use of styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,11 @@
 import Footer from './footer'
 import Navbar from './navbar'
-import styles from '../styles/Layout.module.css'
 // templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
 export default function Layout({ children }) {
 	return (
 		<>
 			<Navbar />
-			<div className={styles.container}>{children}</div>
+			{children}
 			<Footer />
 		</>
 	)
