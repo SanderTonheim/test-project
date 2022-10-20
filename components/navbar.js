868f617128cc4d0ffb605bb537ecacc91a9e0da0
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-20 13:42:03 +0200

    Added logo to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import style from '../styles/Navbar.module.css'
+import Logo from '../asssets/logo.png'
 
 export default function Navbar() {
 	return (
 		<div className={style.navbar}>
+			<Image src={Logo} width={70} height={100} />
+
 			<div className={style.links}>
 				<Link href='/'>Hjem</Link>
 				<Link href='/about'>About</Link>
