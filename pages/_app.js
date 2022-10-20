5154c85f997b3b748756995b647c1458b590558a
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-20 13:42:23 +0200

    Added next head component to all sites

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
+import Head from 'next/head'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 // Wrapper layout komponenten rundt slik at Navbar layouten kommer på hver side som vises.
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Head>
+				<title>Prøve prosjekt</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	)
 }
 
