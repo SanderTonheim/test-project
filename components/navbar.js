2fef512a3919ffb7e245ae8476daf2ce60abeecc
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-21 10:02:28 +0200

    Cleanup

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import style from '../styles/Navbar.module.css'
+import styles from '../styles/Navbar.module.css'
 import Logo from '../asssets/logo.png'
 
 export default function Navbar() {
 	return (
-		<div className={style.navbar}>
+		<div className={styles.navbar}>
 			<Image src={Logo} width={70} height={100} />
 
-			<div className={style.links}>
+			<div className={styles.links}>
 				<Link href='/'>Hjem</Link>
 				<Link href='/about'>About</Link>
 				<Link href='/prosjektet'>Prosjektet</Link>
