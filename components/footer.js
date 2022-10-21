ca35c904c06575af89adc459b645ca4edf184a8d
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-21 10:00:50 +0200

    Cleanup

--- components/footer.js
+++ components/footer.js
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import styles from '../styles/Footer.module.css'
 export default function Footer() {
 	return (
-		<div className={styles.container}>
-			<div className={styles.footer}>
+		<div className={styles.footer}>
+			<div className={styles.text}>
 				<p>Test prosjekt </p>
 				<p>2022</p>
 				<p>Sander Tonheim</p>
 			</div>
+
 			<div className={styles.links}>
 				<Link href={'/'}>Hjem</Link>
 				<Link href={'/about'}>about</Link>
