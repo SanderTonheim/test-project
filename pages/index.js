efe327201618038270b21a0ee805efe853bcb2da
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-21 10:03:14 +0200

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 	return (
 		<>
-			<Head></Head>
+			
 			<div className={styles.container}>
-				<h1>Velkommen </h1>
-				<h1>Velkommen </h1>
 				<h1>Velkommen </h1>
 				<h1>Velkommen </h1>
 				<h1>Velkommen </h1>
