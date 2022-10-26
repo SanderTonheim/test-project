b045c658a4ead621b40fe925b69a70ed1e533cd3
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-26 12:14:44 +0200

    comment

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,44 +1,72 @@
-// Dette er en dynamisk route som vil si at contentet på siden vil varie utifra hvilke "username" som kommer etter profile feks "side.no/profile/Sander". Oppsettet på sidene vil være likt men inholde ("hei (noe)") vil endre seg i dette tilfellet.
+// [] rundt navnet sier til next at dette er en side som er dynamisk. Layouten endrer seg ikke, men innholdet endrer seg utifra hvilke id funksjonen mottar.
+// denne filer er en "templet" for hvordan "profile/[id]" siden skal se ut.
+// Her skriver vi også hvilke sider som skal bli generert på serveren når siden skal bygget til produksjon.
 
-// Importerer useRouter slik at vi får tilgang til siste delen på URL. Feks "side.no/profile/*Sander *" også kjent som parameter.
-import Axios from 'axios'
-
+// kjører når applikasjonen skal bli bygget for produskjon.
+// getStatic paths er en funksjon som gennerer 1 rute og 1 html side for hver id. feks side.no/profile/1.
+// Må være async funksjon slik at vi kan få tak i alle id verdiene og returnere de til next slik at next vet hvor mange sider og ruter den skal generere.
 export const getStaticPaths = async () => {
+	// hent samme data som i getStaticProps i index.js for å få tak i alle id.
+	// må bruke fetch()
+	// res = await = vent til API svarer
 	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	// converterer svarer fra API til JSON fil.
+	// const data = await res.json = vent til filen er ferdig konvertert.
 	const data = await res.json()
+	// map funkjson med navn paths.
 
 	const paths = data.map((profile) => {
+		//for hvert element i objektet data, returner objekt med navn "params" som inneholder en variabel med navn "id".
+		// id objektet har verdi profile.id.toString(). Dette henter id feltet for hvert objekt og konverterer det til en string.
 		return {
 			params: { id: profile.id.toString() },
 		}
 	})
 
+	//returner variabel som next kan se på for å finne ut hvor mange sider og ruter den skal lage når applikasjonen blir bygget.
 	return {
+		// paths henter ut alle id for hvert objekt fra responsen fra API'en. slik at next genererer en side og rute for hver id i listen.
 		paths,
 		fallback: false,
 	}
 }
 
+// kjører når applikasjonen skal bli bygget for produskjon.
+// funksjon for å hente ut data om en spesifik profil.
+// funksjonen vil kjøre for hver profil side som skal bli generert av next. Den referer til paths fra forige funksjon for å vite hvor mange ganger den skal kjøre.
+// feks paths returnerer 10 id'er, denne funkjonen vil kjøre 10 ganger.
+
 export const getStaticProps = async (context) => {
+	// vi får tilgang til parameteret vi trenger via context objektet, som vi aksepterer som et argument.
+	// hver id vi får tak i vil bli festet til context objektet. Feks context.params.id.
+	// context referer til path som igjen referer til return fra map funkjsonen som er et objekt med navn params. Vi får en ny id hver gang fra context objektet.
+	// paths = const path = data.map return params{ id: profile.id.toString() }
 	const id = context.params.id
+
+	// fetch request for hver enkelt id. url/ + id variabel.
+	// await for å vente på svar fra API.
 	const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+
+	// vent til konverteringen av res til json format er ferdig.
 	const data = await res.json()
 
+	// returner et objekt med navn props fra denne funkjsonen. Slik at Details funkjsonen får tilgang til dataen som ble hentet fra fetch.
+	// objekt inneholder profile variabel med verdien til dataen som ble returnet fra fetch.
 	return {
 		props: { profile: data },
 	}
 }
 
+// Fuknker som en templet for hvordan alle side.no/profile/id sider skal se ut.
+// Destrkturer props som ble returnert fra getStaticProps funksjonen. Setter profile som props i Details funksjonen for å få tilgang til dataen.
 const Details = ({ profile }) => {
-	// Dette gjør slik at vi får tilagang til alle funsjonene som ligger under useRouter()hooken.
-	// Feks "router.query".Dette vil gi oss tilgang til et object hvor vi kan hente ut data
-	// avhenging av query på URL.
-	// const router = useRouter()
-
+	// sier hva som skal returneres fra profile.
 	return (
-		<div >
-			{/* viser hei! {URL query teksten} */}
+		// for hver enkelt id side vises en div
+		<div>
+			{/* Viser en <h1> med navnet til bedriften */}
 			<h1> {profile.company.name} </h1>
+			{/* viser tekst og navnet til bedriften */}
 			<p>The owner of this astablishment is {profile.name} </p>
 		</div>
 	)
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,25 +2,50 @@ import Axios from 'axios'
 import Link from 'next/link'
 import styles from '../../styles/profileIndex.module.css'
 
-// for å få tilgang til dataen som ble returnert av API setter vi "profiles" som argument i "ProfileList" funksjonen.
-// verdien til profiles er array'en med profiler/data
+// Først hent data fra API.
+
+// denne funksjonen getStaticProps skal hente data fra en API og returnere et objjekt som andre komponenter kan bruke.
+// for at det skal fungere slik det er ment lager vi en funkjson med navnet getStaticProps som er async.
+export const getStaticProps = async () => {
+	// Henter data fra API'en
+	// await = sett funkjsonen på pause frem til den får et svar, enten som "ok" eller en error.
+	const data = await Axios.get('https://jsonplaceholder.typicode.com/users')
+
+	// Vi sier at "getStaticProps" skal retunerer et objekt med navn "props", som inneholder et object med navn "profiles" som har verdien data.data som er responsen vi fikk fra API'en.
+	return {
+		props: {
+			profiles: data.data,
+		},
+	}
+}
+
+// for å få tilgang til dataen som blir returnert av getStaticProps funksjonen setter vi "profiles" som argument i "ProfileList" funksjonen.
+// verdien til profiles er array'en med profiler/data fra props objektet som ble returnert fra getStaticProps.
+
 const ProfileList = ({ profiles }) => {
 	return (
-		// her sier vi hva vi skal returnere fra komponenten.
-		// returner en div
+		// her sier vi hva vi skal returnere fra ProfileList funkjsonen.
+
+		// returner en div som inneholder map funksjonen.
 		<div className={styles.container}>
-			{/* inni div'en skal det som retuneres fra map funskjonen vises.  */}
 			{/* map funksjon:  vi setter inn et argument som vi kan kalle hva vi vil.*/}
-			{/* Alle objekter som ligger inni "profiles" skal nå bli refert til som profile. eks 0.name="sander" = profile.name="sander"  */}
-			{/* Return fra map funkjsonen sier hva den skal vise for hvert obejkt. "For hver "profile" i listen skal du vise en h1 med tekst fra profile.name variabelen. */}
+			{/* Alle objekter som ligger inni "profiles" skal nå bli refert til som profile. eks data.data[0].name="sander" = profile.name="sander"  */}
+			{/* Return fra map funkjsonen sier hva den skal vise for hvert obejekt. "For hver "profile" i listen skal det vise en h1 med tekst fra profile.name variabelen. */}
 			{profiles.map((profile) => {
-				console.log(profile.id)
 				return (
+					// Lag <h1>
 					<h1>
+						{/*sett <h1> til å fungere som en link/klikkbar  */}
 						<Link
+							// path til hvor linken skal sende brukeren.
+							// send bruker til url/profile(mappe)/ + profile.id som kommer fra objektet.
 							href={'/profile/' + profile.id}
+							// vi gir alle elementer i objekt arrayen sin egen identitet ved bruk at key={id til hver profil}
+							// objekt 1 får key={1}, objekt 2 får key={2} etc.
+							// Dette er for å hjelpe react å holde oversikt om ting endrer seg, blir lagt til eller slettet.
 							key={profile.id}
 						>
+							{/* tekst som skal vises mellom <h1> taggene  */}
 							{profile.company.name}
 						</Link>
 					</h1>
@@ -29,19 +54,5 @@ const ProfileList = ({ profiles }) => {
 		</div>
 	)
 }
-
-// denne funksjonen getStaticProps skal hente data fra en API og returnere en variabel som andre komponenter kan bruke.
-// for at det skal fungere slik det er ment lager vi en funkjson med navnet getStaticProps.
-export const getStaticProps = async () => {
-	// Henter data fra API'en
-	// await = sett funkjsonen på pause frem til du får en verdi.
-	const data = await Axios.get('https://jsonplaceholder.typicode.com/users')
-	// retunerer et objekt med verdi "props", som inneholder et object med verdi "profiles" som representerer dataen vi fikk tilbake fra API'en.
-	return {
-		props: {
-			profiles: data.data,
-		},
-	}
-}
-
+// eksporter funksjonen ProfileList som en komponent.
 export default ProfileList
