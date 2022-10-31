307fdb3b89c69ca6bc747df887b3dee8dab7e870
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-31 11:23:29 +0100

    footer color changed

--- components/footer.js
+++ components/footer.js
@@ -4,15 +4,8 @@ export default function Footer() {
 	return (
 		<div className={styles.footer}>
 			<div className={styles.text}>
-				<p>Test prosjekt </p>
-				<p>2022</p>
+				<p>Test prosjekt 2022</p>
 				<p>Sander Tonheim</p>
-			</div>
-
-			<div className={styles.links}>
-				<Link href={'/'}>Hjem</Link>
-				<Link href={'/about'}>about</Link>
-				<Link href={'/prosjektet'}>prosjektet</Link>
 			</div>
 		</div>
 	)
