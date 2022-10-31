d3d8e23c5972997b99c65c0faf246dcc4954759f
Sander Tonheim <sandert.kodehode@gmail.com>
2022-10-31 15:34:06 +0100

    s

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home(context) {
 						<p>
 							Where can I get some? There are many variations of passages of
 							Lorem Ipsum available, but the majority have suffered alteration
-							in some form, by injected humour, or randomised words which don't
+							in some form, by injected humour, or randomised words which dont
 							look even slightly believable. If you are going to use a passage
 							of Lorem Ipsum, you need to be sure there isn't anything
 							embarrassing hidden in the middle of text. All the Lorem Ipsum
