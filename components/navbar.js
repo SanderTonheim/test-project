122a6d0392f9d5b9b591d8dd9c383e048ed51208
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-02 18:42:45 +0100

    navbar media quary mobil

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Navbar.module.css'
+import s from '../styles/Navbar.module.css'
 import Logo from '../assets/logo.svg'
+import menuIcon from '../assets/menu-icon.png'
 
 export default function Navbar() {
 	return (
-		<div className={styles.navbar}>
-			<div className={styles.logo}>
+		<div className={s.navbar}>
+			<div className={s.logo}>
 				<Image
 					src={Logo}
 					width={1600}
@@ -14,7 +15,14 @@ export default function Navbar() {
 					alt=''
 				/>
 			</div>
-			<div className={styles.links}>
+			<div className={s.menu}>
+				<Image
+					src={menuIcon}
+					width={30}
+					height={25}
+				/>
+			</div>
+			<div className={s.links}>
 				<Link href='/'>Hjem</Link>
 				<Link href='/profile'>About</Link>
 				<Link href='/prosjektet'>Prosjektet</Link>
