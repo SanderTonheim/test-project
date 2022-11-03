ba7e19babc5a3c6ff355f85f2511f2423d7f7da5
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-03 11:53:31 +0100

    Head component added to layout

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,13 @@ import styles from '../styles/Layout.module.css'
 export default function Layout({ children }) {
 	return (
 		<>
+			<Head>
+				<title>Prøve prosjekt</title>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				/>
+			</Head>
 			<Navbar />
 			<div className={styles.content}>{children}</div>
 			<Footer />
