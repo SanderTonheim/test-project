6a89f11be868ab8af70cbe846d461895b7e9302d
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-08 14:23:47 +0100

    CMS hooked up to front end

---
+++ test-project-cms/schemas/medlem.js
@@ -0,0 +1,72 @@
+import LeafletGeopointInput from 'sanity-plugin-leaflet-input'
+
+export default {
+	name: 'medlem',
+	type: 'document',
+	title: 'Medlems bedrift',
+	fields: [
+		{
+			title: 'Navn',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Logo',
+			name: 'logo',
+			type: 'image',
+		},
+		{
+			title: 'Adresse',
+			name: 'address',
+			type: 'string',
+		},
+		{
+			title: 'Post sted',
+			name: 'zip',
+			type: 'string',
+		},
+		{
+			title: 'Kart',
+			name: 'location',
+			type: 'geopoint',
+			inputComponent: LeafletGeopointInput,
+		},
+		{
+			title: 'Kontakt person',
+			name: 'contactPerson',
+			type: 'string',
+		},
+		{
+			title: 'E-post',
+			name: 'email',
+			type: 'string',
+		},
+		{
+			title: 'Telefon',
+			name: 'phone',
+			type: 'number',
+		},
+		{
+			name: 'website',
+			title: 'Nettside',
+			type: 'url',
+		},
+		{
+			title: 'Tags',
+			name: 'tags',
+			type: 'array',
+			of: [{ type: 'string' }],
+		},
+		{
+			title: 'Info',
+			name: 'info',
+			type: 'array',
+			of: [{ type: 'string' }],
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+		},
+	],
+}
