00f7beccdb5f4f2798bb84f6b515a1637d255030
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-11 18:38:23 +0100

    map

--- components/map.js
+++ components/map.js
@@ -1,19 +1,21 @@
-import {
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
   withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
 
-const MapExample = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{lat:24, lng:20}}
-    >
-    </GoogleMap>
-  ))
-);
+<MyMapComponent isMarkerShown />
 
 
-export default MapExample
