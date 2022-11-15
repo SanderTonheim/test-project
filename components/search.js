1a320b946001ad3915c351f4933d39afc4b45a0a
SanderTonheim <98819472+SanderTonheim@users.noreply.github.com>
2022-11-15 18:05:30 +0100

    Add files via upload

---
+++ components/search.js
@@ -0,0 +1,47 @@
+import algoliasearch from 'algoliasearch'
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom'
+import s from '../styles/search.module.css'
+import { useRef, useEffect, useState } from 'react'
+import React from 'react'
+
+export default function Search() {
+	const searchClient = algoliasearch(
+		'BC0Z4HS7B1',
+		'efb4c3162db4848f35d83092db403111'
+	)
+	const Hit = ({ hit }) => <p>{hit.name}</p>
+
+	const refOne = React.forwardRef(false)
+	const handleClickOutSide = (e) => {
+		if (e.targetn.value == '') {
+			setOpen(false)
+		} else {
+			setOpen(true)
+		}
+	}
+
+	console.log(refOne)
+	useEffect(() => {
+		document.addEventListener('keypress', handleClickOutSide)
+	}, [])
+	const [open, setOpen] = useState(false)
+
+	return (
+		<div className={s.container}>
+			<InstantSearch
+				indexName={'Medlem søk'}
+				searchClient={searchClient}
+			>
+				<div className={s.search_hits}>
+					<SearchBox
+						ref={refOne}
+						className={s.input}
+					/>
+					<div className={s.hitContainer}>
+						{open && <Hits hitComponent={Hit} />}
+					</div>
+				</div>
+			</InstantSearch>
+		</div>
+	)
+}
