97ae17eaa3e5ae12bfefe80efa537a5db3abbc76
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-16 15:18:01 +0100

    removed key

--- next.config.js
+++ next.config.js
@@ -8,10 +8,5 @@ module.exports = {
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 	},
 }
-module.exports = {
-	env: {
-		customKey: 'AIzaSyD4UD_3-eGEmZOGaMtA5lgLbLBoU7VVegM',
-	},
-}
 
 module.exports = nextConfig
