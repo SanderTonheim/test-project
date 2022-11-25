d4c9d78a9ff4595648349d57ef5ff62d99608a40
Sander Tonheim <sandert.kodehode@gmail.com>
2022-11-25 11:32:07 +0100

    Made "Certifications" component

---
+++ components/Certifications.js
@@ -0,0 +1,17 @@
+import { urlFor } from '../lib/sanity'
+import s from '../styles/TagList.module.css'
+export default function Certifications({ list }) {
+	return list.map((item) => {
+		return (
+			<div
+				className={s.tag}
+				key={item.name}>
+				<h4>{item.name}</h4>
+				<img
+					src={urlFor(item.image.asset._ref)}
+					alt=''
+				/>
+			</div>
+		)
+	})
+}
