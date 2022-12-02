7ab6a541e807b044cca154efdb3fdeee831762e8
sander <sandert.kodehode@gmail.com>
2022-12-02 22:24:12 +0100

    added section 2, added header in section 2

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,20 @@ const Home = () => {
 	return (
 		<main className={s.mainContainer}>
 			<section className={s.section_1}>
-				<h1 className={s.header} >Næringsrådets Bedriftmedlemmer</h1>
+				<h1 className={s.header}>Næringsrådets Bedriftmedlemmer</h1>
 				<div className={s.searchBG}>
 					<Search />
 				</div>
 			</section>
-			<div className={s.mainSponsors}>
-				<Sponsors />
-			</div>
-			<div className={s.secondarySponsors}>
-				<SecondarySponsors />
-			</div>
+			<section className={s.section_2}>
+				<h2>Våre sammarbeidspartenere</h2>
+				<div className={s.mainSponsors}>
+					<Sponsors />
+				</div>
+				<div className={s.secondarySponsors}>
+					<SecondarySponsors />
+				</div>
+			</section>
 		</main>
 	)
 }
