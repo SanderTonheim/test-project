e61f642db931fad08b424098dbabc1890e362f46
SanderTonheim <98819472+SanderTonheim@users.noreply.github.com>
2022-12-02 20:03:19 +0100

    added certifications and contactperson schema

---
+++ test-project-cms/schemas/certifications.js
@@ -0,0 +1,18 @@
+export default {
+	name: 'certifications',
+	type: 'document',
+	title: 'Sertifiseringer',
+
+	fields: [
+		{
+			title: 'Navn',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Bilde',
+			name: 'icon',
+			type: 'image',
+		},
+	],
+}
---
+++ test-project-cms/schemas/contactPerson.js
@@ -0,0 +1,27 @@
+export default {
+	title: 'Kontakt person',
+	name: 'contactPerson',
+	type: 'document',
+	fields: [
+		{
+			title: 'Avdeling',
+			name: 'department',
+			type: 'string',
+		},
+		{
+			title: 'Navn',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Epost',
+			name: 'mail',
+			type: 'string',
+		},
+		{
+			title: 'Telefon',
+			name: 'phone',
+			type: 'number',
+		},
+	],
+}
