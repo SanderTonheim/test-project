4b8b6d729e78c685ea58444b047b707eb6cc8d35
sander <sandert.kodehode@gmail.com>
2022-12-03 01:43:50 +0100

    added certifications to mainSponsors front page

--- components/mainSponsors.js
+++ components/mainSponsors.js
@@ -6,7 +6,8 @@ import urlFor from '../lib/sanity'
 
 export default function mainSponsors() {
 	const [data, setData] = useState([])
-	const URL = 'https://8rpris03.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22medlem%22%20%26%26%20%20MainSponsor%20%3D%3D%20true%5D'
+	const URL =
+		'https://8rpris03.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22medlem%22%20%26%26%20%20MainSponsor%20%3D%3D%20true%5D%7Bname%2C%20_id%2C%20logo%2C%20certifications%5B%5D-%3E%7D'
 	const getMainSponsors = () => {
 		axios.get(URL).then((res) => {
 			return setData(res.data.result)
@@ -27,9 +28,15 @@ export default function mainSponsors() {
 							src={urlFor(item.logo.asset._ref)}
 							alt=''
 						/>
-						<div className={s.tagContainer}>
-							{item.tagList?.map((tag) => {
-								return <p className={s.tags}>{tag}</p>
+
+						<div className={s.certifications}>
+							{item.certifications?.map((cert) => {
+								return (
+									<img
+										src={urlFor(cert.icon)}
+										alt=''
+									/>
+								)
 							})}
 						</div>
 					</div>
