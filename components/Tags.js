a1be4b4f1671118a4c4c0b4077ccedd573879a7c
Sander Tonheim <sandert.kodehode@gmail.com>
2022-12-05 12:45:51 +0100

    added key props

--- components/Tags.js
+++ components/Tags.js
@@ -4,7 +4,7 @@ export default function Tags({ list }) {
 	return list.map((tag) => {
 		console.log(tag)
 		return (
-			<ul>
+			<ul key={tag._key}>
 				<li>{tag.tagList}</li>
 				<div>{urlFor(tag.tagIcon.asset._ref)}</div>
 			</ul>
