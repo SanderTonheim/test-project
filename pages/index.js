ec0c438c13ce017e8c15b193b5fc3acad377fbad
Sander Tonheim <sandert.kodehode@gmail.com>
2022-12-19 13:29:38 +0100

    added figure to front page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import s from '../styles/Home.module.css'
 import Search from '../components/search'
 import MainSponsors from '../components/MainSponsors'
-import arrow from '../assets/arraow.svg'
+import arrow from '../assets/arrow.svg'
 import Image from 'next/image'
 import { Autocomplete } from '@react-google-maps/api'
+import figure from '../assets/frontPageFigure.png'
 
 const Home = () => {
 	return (
 		<main className={s.mainContainer}>
 			<section className={s.section_1}>
 				<h1 className={s.header}>Næringsrådets Bedriftmedlemmer</h1>
-				<div className={s.searchBG}>
-					<Search />
+				<Search />
+				<div className={s.figure}>
+					<img
+						src={figure.src}
+						alt='figure'
+					/>
 				</div>
 			</section>
 
