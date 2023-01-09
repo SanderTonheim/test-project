8598bf8200e90a7f9337b395cc79ed107488140f
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-09 15:24:45 +0100

    added div for footer placement issue

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,13 @@
 import Footer from './footer'
 import Navbar from './navbar'
-import styles from '../styles/layout.module.css'
+import s from '../styles/layout.module.css'
 import Head from 'next/head'
 // templet komponent som man wrapper rundt andre komponenter slik. Kan brukes flere ganger
 export default function Layout({ children }) {
 	return (
 		<>
 			<Navbar />
-			{children}
+			<div className={s.content}>{children}</div>
 			<Footer />
 		</>
 	)
