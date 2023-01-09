369a7de1d9d920fc1f36f4d3895169c7c28f3dd4
Sander Tonheim <sandert.kodehode@gmail.com>
2023-01-09 15:43:02 +0100

    removed head component

--- pages/_app.js
+++ pages/_app.js
@@ -5,22 +5,9 @@ import '../styles/globals.css'
 // Wrapper layout komponenten rundt slik at Navbar layouten kommer på hver side som vises.
 function MyApp({ Component, pageProps }) {
 	return (
-		<>
-			<Head>
-				<title>Prøve prosjekt</title>
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0'
-				/>
-				<link
-					rel='icon'
-					href='/favicon.ico'
-				/>
-			</Head>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
 	)
 }
 
